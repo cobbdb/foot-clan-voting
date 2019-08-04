b01b5d19009eabbb694682cb26fe2e29b00ef796
Dan Cobb <cobbdb@gmail.com>
2019-08-04 01:15:22 -0700

    .

---
+++ build/precache-manifest.177798eda5b05556ed20ff55e321ab5a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "424267afdaa07df40364f6c3d25baf0d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ee6ab3f2da599c45b52",
+    "url": "/static/css/main.d470e2fa.chunk.css"
+  },
+  {
+    "revision": "1c4a896e48005e26d783",
+    "url": "/static/js/2.f2bf1e59.chunk.js"
+  },
+  {
+    "revision": "3ee6ab3f2da599c45b52",
+    "url": "/static/js/main.4fca7cad.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3af24e4b320023e4d8fbae07a8c2bc84",
+    "url": "/static/media/class-icons.3af24e4b.png"
+  },
+  {
+    "revision": "b34725995c10304463a5a40e15b209b5",
+    "url": "/static/media/faction-icons.b3472599.png"
+  },
+  {
+    "revision": "ed6fc28fdd1d04b69271c5c1ef9482ad",
+    "url": "/static/media/game-icons.ed6fc28f.png"
+  }
+]);
